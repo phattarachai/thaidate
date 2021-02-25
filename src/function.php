dd8240a87ca26e37bf0271c2c094fde9fbf231c2
Phattarachai Chaimongkol <phatchai28@gmail.com>
2021-02-25 23:34:44 +0700

    Develop (#1)
    * Add thaidate() function
    * Add Laravel supported.
    * Add Tests
    * Add Readme

---
+++ src/function.php
@@ -0,0 +1,22 @@
+<?php
+
+use Phattarachai\Thaidate\Thaidate;
+
+
+/**
+ * Thai date based on php built-in date() function.
+ *
+ * @param string $format The format as same as PHP date function format. See http://php.net/manual/en/function.date.php
+ * @param string|int $timestamp The optional timestamp is an integer Unix timestamp.
+ * @param boolean $buddhistEra Use Buddhist era? set to true to use that or false not to use.
+ * @return string Return the formatted date/time string.
+ */
+function thaidate(string $format = 'j F Y', $timestamp = 'now', bool $buddhistEra = true)
+{
+    if (is_string($timestamp)) {
+        $timestamp = strtotime($timestamp);
+    }
+
+    return (new Thaidate)->date($format, $timestamp, $buddhistEra);
+}
+
